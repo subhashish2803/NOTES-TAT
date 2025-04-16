ba22c56636b99096ac8f202c9d2b3f9962c5edb7
ashu <nayaksubhashish456@gmail.com>
2025-04-16 22:03:12 +0530

    Updated Navbar: added Vistofy brand and enhancements

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -43,56 +43,108 @@ export default function Header() {
   React.useEffect(() => {
     const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
     const handleChange = (event) => setIsDark(event.matches ? true : false);
+
     mediaQuery.addEventListener("change", handleChange);
+
     return () => {
       mediaQuery.removeEventListener("change", handleChange);
     };
   }, []);
 
-  return (
-    <Navbar
-      sticky="top"
-      className={`navbar navbar-expand-lg py-4 cdin ${isDark ? "navbar-dark" : ""}`}
-      expand="lg"
-    >
-      <Container>
-        <Nav>
-          <Navbar.Brand>
-            <Link to="/" className="d-flex align-items-center gap-2 text-decoration-none">
-              <img
-                src={isDark ? logo1 : logo2}
-                alt="logo"
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-              />
-              <h5 className={isDark ? "text-white" : "text-dark mt-2"} >VISTOFY</h5>
-            </Link>
-          </Navbar.Brand>
-        </Nav>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0">
-            <Link to="/notes" className="nav-link">
-              Notes
-            </Link>
-          </Nav>
+  if (isDark) {
+    return (
+      <Navbar
+        sticky="top"
+        className="navbar navbar-expand-lg navbar-dark py-4 cdin"
+        expand="lg"
+      >
+        <Container>
           <Nav>
-            <Link to="/team" className="nav-link">
-              About Us
-            </Link>
-            {currentUser ? (
-              <Link to="/profile" className="nav-link">
-                {buttonText}
+            <Navbar.Brand>
+              <Link to="/">
+                <img
+                  src={logo1}
+                  alt="logo"
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                />
               </Link>
-            ) : (
-              <Link to="/login" className="nav-link">
-                {buttonText}
-              </Link>
-            )}
+              
+            </Navbar.Brand>
           </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="me-auto my-2 my-lg-0">
+              <Link to="/notes" className="nav-link">
+                Notes
+              </Link>
+            </Nav>
+            <Nav>
+              <Link to="/team" className="nav-link">
+                About Us
+              </Link>
+              {currentUser ? (
+                <Link to="/profile" className="nav-link">
+                  {buttonText}
+                </Link>
+              ) : (
+                <Link to="/login" className="nav-link">
+                  {buttonText}
+                </Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  } else {
+    return (
+      <Navbar
+        sticky="top"
+        className="navbar navbar-expand-lg py-4 cdin"
+        expand="lg"
+        
+      >
+        <Container>
+          <Nav>
+            <Navbar.Brand>
+              <Link to="/">
+                <img
+                  src={logo2}
+                  alt="logo"
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                />
+              </Link>
+            </Navbar.Brand>
+          </Nav>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+          
+            <Nav className="me-auto my-2 my-lg-0">
+              <Link to="/notes" className="nav-link">
+                Notes
+              </Link>
+            </Nav>
+            <Nav>
+              <Link to="/team" className="nav-link">
+                About Us
+              </Link>
+              {currentUser ? (
+                <Link to="/profile" className="nav-link">
+                  {buttonText}
+                </Link>
+              ) : (
+                <Link to="/login" className="nav-link">
+                  {buttonText}
+                </Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
 }
